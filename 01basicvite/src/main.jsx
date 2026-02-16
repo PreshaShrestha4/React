45f72f0616c70ff12989dfb35ce75ee3b5bf47d2
PreshaShrestha4 <preshashrestha041@gmail.com>
2026-02-16 14:43:49 +0545

    update

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const AnotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+const AReactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "click me to visit google",
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<MyApp />);
